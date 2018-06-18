c9faa6b3c3dd7005e4d9e416ee412a9e0a7885ae
Sophie Nguyen <38814441+furtivesock@users.noreply.github.com>
2018-06-18 17:21:16 +0200

    Update caracteristiques-ondes.php

--- caracteristiques-ondes.php
+++ caracteristiques-ondes.php
@@ -19,7 +19,7 @@
     <div class="contenu">
         <!-- PRESENTATION -->
         <div class="dialogue">
-            <img class="dialogue-icone" src="images/lac-fleur.png" width="15%" height="15%">
+            <img class="dialogue-icone" src="images/lac-fleur.PNG" width="15%" height="15%">
             <div class="dialogue-box" id="dialogue-qcm">
                 <div class="dialogue-texte">
                     <?php 
